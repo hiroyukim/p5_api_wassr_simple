6f87266be44e2ea10992f08c4da3151e7a86042b
hiroyuki <hiroyukimm@gmail.com>
2012-01-23 21:19:30 +0900

    set up

---
+++ lib/Api/Wassr/Simple.pm
@@ -0,0 +1,32 @@
+package Api::Wassr::Simple;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Api::Wassr::Simple -
+
+=head1 SYNOPSIS
+
+  use Api::Wassr::Simple;
+
+=head1 DESCRIPTION
+
+Api::Wassr::Simple is
+
+=head1 AUTHOR
+
+yamanaka hiroyuki E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
